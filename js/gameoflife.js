2edca8965f7c3ece1c201b3692c71a57d6baedec
Brian Kungu <bkungu07@gmail.com>
2021-12-06 17:25:32 +0300

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,11 +1,19 @@
-function seed() {}
+function seed() {
+  return Array.prototype.slice.call(arguments);
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return x === j && y === k;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return this.some((c) => same(c,cell));
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  return contains.call(state, cell) ? "\u25A3" : "\u25A2";
+};
 
 const corners = (state = []) => {};
 
